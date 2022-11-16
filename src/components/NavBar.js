341c1fd028fa47e429590166bd5f74c2750812ce
BuntyBru <buntybru@gmail.com>
2022-11-17 00:00:04 +0530

    changed

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,14 +3,13 @@ import Logo from "./NavbarComponents/Logo"
 import CTASection from "./NavbarComponents/CTASection"
 import { navbar } from "../styles/navbar.module.css"
 
-const NavBar = () => {
+const Navbar = () => {
   return (
     <div className={navbar}>
       <Logo />
-
       <CTASection />
     </div>
   )
 }
 
-export default React.memo(NavBar)
+export default React.memo(Navbar)
